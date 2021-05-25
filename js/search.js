07d270f5071125321d0e149b185494cc647e5189
CESAR FERNANDEZ <cesarfernandez@iMac-de-CESAR.local>
2021-05-25 00:55:38 -0500

    search mejorado ECMAScript y css, responsive agregado

--- js/search.js
+++ js/search.js
@@ -1,53 +1,61 @@
 const datos_buscar = ["Imagen", "Fotografía", "Diseño", "Color", "Concepto", "Idea", "Creatividad", "Talento"];
 
 function autocomplete(inp, arr) {
-  var currentFocus;
+  let currentFocus;
+
   inp.addEventListener("input", function(e) {
-      var a, b, i, val = this.value;
+      let a, b, i, val = this.value;
       closeAllLists();
+
       if (!val) { return false;}
+
       currentFocus = -1;
-      a = document.createElement("DIV");
+
+      a = document.createElement("div");
       a.setAttribute("id", this.id + "autocomplete-list");
       a.setAttribute("class", "autocomplete-items");
       this.parentNode.appendChild(a);
-      for (i = 0; i < arr.length; i++) {
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+
+      arr.forEach(function (arr, index) {
+        console.log(arr)
+        console.log(index)
+
+        if (arr.substr(0, val.length).toUpperCase() == val.toUpperCase()) {
           b = document.createElement("DIV");
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+          b.innerHTML = "<strong>" + arr.substr(0, val.length) + "</strong>";
+          b.innerHTML += arr.substr(val.length);
+          b.innerHTML += "<input type='hidden' value='" + arr + "'>";
           b.addEventListener("click", function(e) {
               inp.value = this.getElementsByTagName("input")[0].value;
               closeAllLists();
           });
           a.appendChild(b);
         }
-      }
+      });
   });
   
   inp.addEventListener("keydown", function(e) {
-      var x = document.getElementById(this.id + "autocomplete-list");
+      let x = document.getElementById(this.id + "autocomplete-list");
       if (x) x = x.getElementsByTagName("div");
+      console.log(e.keyCode)
       if (e.keyCode == 40) {
-        
         currentFocus++;
-        
         addActive(x);
-      } else if (e.keyCode == 38) { 
+      }
+      else if (e.keyCode == 38) { 
         currentFocus--;
-        
         addActive(x);
-      } else if (e.keyCode == 13) {
-        
+      }
+      else if (e.keyCode == 13) {  
         e.preventDefault();
         if (currentFocus > -1) {
-          
           if (x) x[currentFocus].click();
         }
       }
   });
+
   function addActive(x) {
+    console.log(x)
     if (!x) return false;
     removeActive(x);
     if (currentFocus >= x.length) currentFocus = 0;
